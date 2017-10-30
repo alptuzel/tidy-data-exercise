2d6f3c73012b8d7c4590772491cc80c7606c400d
alptuzel <alptuzel@gmail.com>
2017-10-30 09:05:30 +0000

    commits

--- run_analysis.R
+++ run_analysis.R
@@ -58,6 +58,9 @@ merged_labels<-mutate(merged_labels ,activity_name =
                         factor( merged_labels$V1 , labels = activity_labels$V2))
 
 
+## Make clear variable names by renaming columns
+## Grep used to rename column names which has 2 or more "." with just 1 "."
+
 colnames(merged_labels)[1] <- "Activity.Type"
 
 colnames(merged_labels)[2] <- "Activity.Name"
@@ -67,6 +70,8 @@ colnames(merged_set_mean_std) <- sub("\\.{2,}", "\\.", colnames(merged_set_mean_
 
 
 
+### Add new columns to our date set with Activity name and Subject Ids
+
 merged_set_mean_std <- mutate( merged_set_mean_std , 
                                    Subject.ID= merged_subjects$V1)
 
@@ -74,9 +79,20 @@ merged_set_mean_std <- mutate( merged_set_mean_std ,
 merged_set_mean_std <- mutate( merged_set_mean_std , 
                                Activity= merged_labels$Activity.Name)
 
+
+## Make "group by" to be able to use summarise function
+
 merged_set_ms_group <- group_by(merged_set_mean_std, Activity, Subject.ID )
 
 
+
+## Summarise data with mean function, it is used with  grouped columns
+
 my_tidy_data<- summarise_all(merged_set_ms_group, funs (mean) )
+
+
+## Write final data set to a CSV
+
+write.csv(my_tidy_data,"my_tidy_data.csv")
 
 
